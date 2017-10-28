b78ac393d9da60b69d82eecba62276267ba2c0eb
Vishal Vishwanathan <vishalviswanathan9@gmail.com>
2017-10-28 10:56:17 +0530

    Updated app.js to handle success and error calls from POST requests

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.controller('myCtrl', function($scope,$http){
         console.log(a);
         console.log(b);
         var data = { Username:a, Password:b };
-        $http.post('/server',data).then(function(response){
-                console.log("Posted Successfully"+response);
-            }).catch(function(response){
-                console.log(response);
-        })
+        $http.post('/server',data).success(function(d){
+                console.log("Retrieved Data"+JSON.stringify(d));
+            }).error(function(error){
+                console.log(error);
+        });
     }
 });
